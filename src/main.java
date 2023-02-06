023d2266ca5220324b043727c4bb5041e03d74a7
salmasha <salma@aus.edu>
2023-02-06 17:57:48 +0400

    Feature B added

--- src/main.java
+++ src/main.java
@@ -3,6 +3,10 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		FeatureB();
 	}
-
+public static void FeatureB()
+{
+	System.out.println("Feature B added");
+}
 }
